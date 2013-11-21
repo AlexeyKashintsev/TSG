4ce48861311b6c8861d47f44116ac42949edc3e3
Alexey Kashintsev <AKashintsev@altsoft.biz>
2013-11-21 16:58:25 +0400

    Все характеристики для расчета

--- src/Расчеты/Запросы/Все характеристики для расчета.sql
+++ src/Расчеты/Запросы/Все характеристики для расчета.sql
@@ -5,12 +5,10 @@
  * @manual 
  */ 
 Select * 
-From per_sums t1
-, Calc_object q
+From Calc_object q
  Inner Join lc_flat_services t on q.lc_id = t.lc_id
- and t.lc_flat_services_id = t1.flat_service_id
  Left Join grp_services t2 on t2.services_id = t.services_id
  and t2.group_id = q.group_id
  Left Join grp_chars t3 on t3.grp_group_id = t2.group_id
  Left Join lc_chars t4 on t4.lc_id = t.lc_id
- Where :dateid = t1.date_id
+ Inner Join char_types t1 on t4.lc_char_type = t1.char_types_id
