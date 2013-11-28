2d02a165b4e82f5fb3c5f4be3168cbf05a14b49c
Alexey Kashintsev <AKashintsev@altsoft.biz>
2013-11-28 13:04:41 +0400

    Поправил запрос

--- src/Тарифы/Запросы/Недостающие в тарифах услуги.sql
+++ src/Тарифы/Запросы/Недостающие в тарифах услуги.sql
@@ -5,10 +5,8 @@
  */ 
 Select grp_services_id as services_id
 From grp_services m
-Where  not exists (Select services_id
+Where m.services_id not in (Select services_id
 From usl_tarif d
- Where d.services_id = m.grp_services_id
- and :parDateID = d.date_id
- and :parGroupID = d.group_id
- and :parGroupID = m.group_id
-)
+ Where  :parDateID = d.date_id
+    and :parGroupID = d.group_id
+) and :parGroupID = m.group_id
