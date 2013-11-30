9f1c917daf0d68f8e06f31165740f691212d1d00
ANRI92 <ansadin@mail.ru>
2013-11-30 17:38:48 +0300

    Переделана Дата.Косяки с тарифами:В модель модуля тарифов не добавляется запрос на вывод недостающих сервисов,запутался куда добавлять новый тариф

--- src/Тарифы/ModuleTarifs.js
+++ src/Тарифы/ModuleTarifs.js
@@ -10,23 +10,22 @@
  * @param {type} parDateID
  * @param {type} parGroupID
  * @returns {@exp;services_by_group}
+ * TODO Доделать добавление,запутался куда добавлять;
  */
-function addNewTarifs()
-{   for (i=0;i<services_by_group.params. length;i++)
-        {for (j=0;j<tarifsInGroup.length;j++)
-            {  s=0;
-             if(services_by_group.grp_services_id === tarifsInGroup.services_id)
-                s+=1;
-            }
-         if(s===0)
-            {dstarifsInGroup.insert(dstarifsInGroup.md.services_id,dsservices_by_group.grp_services_id, 
-                                    dstarifsInGroup.md.date_id, parDate,
-                                    dstarifsInGroup.md.group_id,parGroup,
-                                    dstarifsInGroup.md.rate,dstarifsInGroup.rate.last,
-                                    dstarifsInGroup.md.norm,dsdstarifsInGroup.norm.last);
+function addNewTarifs(aDateID,aGroupID){  
+    insertServicesIsAbsent.params.Date = aFlatID;
+    insertServicesIsAbsent.params.Group = aGroupID;
+    insertServicesIsAbsent.execute();
+    insertServicesIsAbsent.beforeFirst();
+         while (insertServicesIsAbsent.next()){
+            dstarifsInGroup.insert(dstarifsInGroup.md.services_id,dsservices_by_group.grp_services_id, 
+                             dstarifsInGroup.md.date_id, parDate,
+                             dstarifsInGroup.md.group_id,parGroup,
+                             dstarifsInGroup.md.rate,dstarifsInGroup.rate.last,
+                             dstarifsInGroup.md.norm,dsdstarifsInGroup.norm.last);}
             }
             
-        }
+       
     model.save();    
-}
+
 
