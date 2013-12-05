8ef64f0eca78b4b9e60aa6e051e936aea28a6b58
ANRI92 <ansadin@mail.ru>
2013-12-05 13:41:54 +0300

    0

---
+++ src/Тарифы/Запросы/Недостающие тарифы.sql
@@ -0,0 +1,13 @@
+/**
+ * @public
+ * @author Андрей
+ * @name ServicesIsAbsent
+ * @manual
+ */
+Select grp_services_id as services_id
+From grp_services m
+Where m.services_id not in (Select services_id
+From usl_tarif d
+ Where  :parDateID = d.date_id
+    and :parGroupID = d.group_id
+) and :parGroupID = m.group_id
