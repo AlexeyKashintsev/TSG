42480a67ebd4dde3c3aef0f3de317a570148fcf0
Alexey Kashintsev <AKashintsev@altsoft.biz>
2013-12-11 18:13:51 +0400

    smallOne (reverted from commit 4e1a78a683aca7caa05768fd73acf4bba00098d3)

---
+++ src/Расчеты/Запросы/Объекты расчета.sql
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author Alexey
+ * @name Calc_object
+ * @manual 
+ */ 
+
+Select * 
+From lc_flat t1
+ Inner Join grp_lc_group t on t.lc_id = t1.lc_flat_id
+ Where (:groupid = t.group_id or :groupid is null)
+ and (:lc_id = t1.lc_flat_id or :lc_id is null)
