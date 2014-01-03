46c0077d4882b0a172974b8e28d56dfae046166e
Alexey Kashintsev <AKashintsev@altsoft.biz>
2014-01-03 21:40:14 +0400

    123

---
+++ src/test.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author Alexey
+ * @name test_con
+ * @public
+ */
+
+
+    Logger.warning('Created');
+    /*
+     * @public
+     */
+    function test(aTestParam){
+        Logger.warning(aTestParam);
+        return aTestParam;
+    };
---
+++ src/Группы/Запросы/Все реквизиты.sql
@@ -0,0 +1,8 @@
+/**
+ *
+ * @author Alexey
+ * @name bank_queries
+ * @public
+ */ 
+Select t1.grp_fname||':'||t1.bank_name as fname, * 
+From grp_bank t1
