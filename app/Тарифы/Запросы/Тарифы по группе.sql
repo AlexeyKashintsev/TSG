46707cb0486f0f98def7acd2454ed8d9d25b2e6f
Alexey Kashintsev <AKashintsev@altsoft.biz>
2015-01-16 16:33:05 +0300

    hotfix 2

--- app/Тарифы/Запросы/Тарифы по группе.sql
+++ app/Тарифы/Запросы/Тарифы по группе.sql
@@ -5,7 +5,9 @@
  * @public
  * @writable usl_tarif
  */ 
-Select *, Case  When t.grp_parent = :parGroup Then t.grp_name else '' End AS child 
+Select t1.usl_tarif_id, t1.date_id, t1.group_id, t1.norm, t1.rate, t1.services_id
+, t1.usr_context
+, Case  When t.grp_parent = :parGroup Then t.grp_name else '' End AS child 
 From usl_tarif t1
  Inner Join grp_groups t on t.grp_groups_id = t1.group_id
  Inner Join grp_services t2 on t2.group_id = t1.group_id
