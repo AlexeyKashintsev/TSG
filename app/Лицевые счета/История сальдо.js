a0fc892e72a133e3128cbd717969b7a6130cb495
ANRI92 <ansadin@mail.ru>
2015-02-25 15:00:41 +0400

    Конечная версия

--- app/Лицевые счета/История сальдо.js
+++ app/Лицевые счета/История сальдо.js
@@ -6,9 +6,14 @@
  */
 
 function formSaldoHistory() {
-var self = this;
+var self = this, model = self.model;
 
-
+self.syncParams = function(aDate, anIsEditable, anAccount) {
+    model.params.parAccountID = anAccount;
+    model.requery();
+    //model.params.parDateID = aDate;
+    //model.params.parAccountID = anAccount;
+};
 
     function btnSaveActionPerformed(evt) {//GEN-FIRST:event_btnSaveActionPerformed
         self.model.save();
