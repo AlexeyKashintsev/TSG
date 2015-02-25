591fb94d28730cd06bc4ae58595b2b439e52df31
Alexey Kashintsev <akashintsev@yandex.ru>
2015-02-25 13:50:26 +0300

    Управление датами форматирование

--- app/Периодика/Дата.js
+++ app/Периодика/Дата.js
@@ -8,86 +8,92 @@
 function fmDateSelector() {
 
 
-var self = this, model = self.model;
+    var self = this, model = self.model;
 
 
-var scrolled = false;
-var changed = false;
-self.parentForm = null;
-var deForm = null;
+    var scrolled = false;
+    var changed = false;
+    self.parentForm = null;
+    var deForm = null;
 
 
 function btn_prevActionPerformed(evt) {//GEN-FIRST:event_btn_prevActionPerformed
-    self.all_dates.prev();              
+        self.all_dates.prev();              
 }//GEN-LAST:event_btn_prevActionPerformed
 
 function all_datesOnScrolled(evt) {//GEN-FIRST:event_all_datesOnScrolled
- //   if (!changed){
-        self.parDateID = self.all_dates.per_date_id;
-        self.parEditDate = self.all_dates.edit_date;
- //       scrolled =  true;}
- //   else {
- //       changed = false;}
-       
-    if (self.all_dates.rowIndex == 1)
-        self.btn_prev.enabled = false;
-    else self.btn_prev.enabled = true;
-    
-    if (self.all_dates.rowIndex == self.all_dates.length )
-        self.btn_last.enabled = self.btn_next.enabled = false;
-    else self.btn_last.enabled = self.btn_next.enabled = true;
-    paramSynchronizer.setDate(self.parDateID, self.parEditDate);    
+        //   if (!changed){
+        if (self.parDateID !== self.all_dates.per_date_id) {
+            self.parDateID = self.all_dates.per_date_id;
+            self.parEditDate = self.all_dates.edit_date;
+        }
+        //       scrolled =  true;}
+        //   else {
+        //       changed = false;}
+
+        if (self.all_dates.rowIndex == 1)
+            self.btn_prev.enabled = false;
+        else
+            self.btn_prev.enabled = true;
+
+        if (self.all_dates.rowIndex == self.all_dates.length)
+            self.btn_last.enabled = self.btn_next.enabled = false;
+        else
+            self.btn_last.enabled = self.btn_next.enabled = true;
+        paramSynchronizer.setDate(self.parDateID, self.parEditDate);    
 }//GEN-LAST:event_all_datesOnScrolled
 
 function btn_nextActionPerformed(evt) {//GEN-FIRST:event_btn_nextActionPerformed
-    self.all_dates.next();    
+        self.all_dates.next();    
 }//GEN-LAST:event_btn_nextActionPerformed
 
 function btn_lastActionPerformed(evt) {//GEN-FIRST:event_btn_lastActionPerformed
-    model.all_dates.last();
+        model.all_dates.last();
 }//GEN-LAST:event_btn_lastActionPerformed
 
 function paramsOnChanged(evt) {//GEN-FIRST:event_paramsOnChanged
-    if (!scrolled){
-        changed =  true; 
-        self.all_dates.scrollTo(self.all_dates.findById(self.parDateID));
-        paramSynchronizer.setDate(self.parDateID, self.parEditDate); 
-           }
-        else 
-        scrolled = false;
-        
-    if (self.all_dates.rowIndex == 1)
-        self.btn_prev.enabled = false;
-    else self.btn_prev.enabled = true;
-    
-    if (self.all_dates.rowIndex == self.all_dates.length )
-        self.btn_last.enabled = self.btn_next.enabled = false;
-    else self.btn_last.enabled = self.btn_next.enabled = true;
-    
+        if (!scrolled) {
+            changed = true;
+            self.all_dates.scrollTo(self.all_dates.findById(self.parDateID));
+            //paramSynchronizer.setDate(self.parDateID, self.parEditDate);
+        }
+        else
+            scrolled = false;
+
+        if (self.all_dates.rowIndex == 1)
+            self.btn_prev.enabled = false;
+        else
+            self.btn_prev.enabled = true;
+
+        if (self.all_dates.rowIndex == self.all_dates.length)
+            self.btn_last.enabled = self.btn_next.enabled = false;
+        else
+            self.btn_last.enabled = self.btn_next.enabled = true;
+
          
 }//GEN-LAST:event_paramsOnChanged
 
 function formWindowOpened(evt) {//GEN-FIRST:event_formWindowOpened
-    model.all_dates.last();
-    all_datesOnScrolled(evt);
+        model.all_dates.last();
+        all_datesOnScrolled(evt);
 }//GEN-LAST:event_formWindowOpened
 
 function btn_newActionPerformed(evt) {//GEN-FIRST:event_btn_newActionPerformed
-    /*self.all_dates.last();
-    var lastDate = self.all_dates.per_date;
-    self.all_dates.insert(self.all_dates.schema.per_date, lastDate.setMonth(lastDate.getMonth()+1));
-    //self.all_dates.scrollTo(self.all_dates.findById(self.all_dates.per_date_id));
-    scrolled = false;
-    self.parDateID = self.all_dates.per_date_id;
-    self.model.save();
-    self.model.requery();
-    self.all_dates.last();*/
-    var newDateProcessor = new DateModule();
-    newDateProcessor.newDate(function(){
-        self.model.requery(function(){
-            model.all_dates.last();
+        /*self.all_dates.last();
+         var lastDate = self.all_dates.per_date;
+         self.all_dates.insert(self.all_dates.schema.per_date, lastDate.setMonth(lastDate.getMonth()+1));
+         //self.all_dates.scrollTo(self.all_dates.findById(self.all_dates.per_date_id));
+         scrolled = false;
+         self.parDateID = self.all_dates.per_date_id;
+         self.model.save();
+         self.model.requery();
+         self.all_dates.last();*/
+        var newDateProcessor = new DateModule();
+        newDateProcessor.newDate(function() {
+            self.model.requery(function() {
+                model.all_dates.last();
+            });
         });
-    });
 }//GEN-LAST:event_btn_newActionPerformed
 
     function btn_confActionPerformed(evt) {//GEN-FIRST:event_btn_confActionPerformed
--- app/Периодика/Управление датами.js
+++ app/Периодика/Управление датами.js
@@ -7,95 +7,101 @@
 function DateModule() {
 
 
-var self = this, model = self.model;
-var grpProcessed = 0;
-var grpModules = [];
-var callBack = null;
+    var self = this, model = self.model;
+    var grpProcessed = 0;
+    var grpModules = [];
+    var callBack = null;
 
-processedCounters = [];
+    processedCounters = [];
 
-/*
- * Вывод предыдущей даты
- * @param {type} parDateID
- * @returns {@exp;per_date}
- * TODO Доделать добавление,запутался куда добавлять;
- */
-self.prevDate = function(aDateID){  
-    self.all_dates.scrollTo(self.all_dates.findById(aDateID));
-    self.all_dates.prev();
-    return self.all_dates.per_date_id;
-};
-
-self.getLastDate = function() {
-    self.model.all_dates.last();
-    return self.model.all_dates.cursor.per_date_id;
-};
-
-self.getPayPeriod4Date = function(aDateID){
-    var curDate = self.all_dates.findById(aDateID).per_pay_day;
-    var prevDate = self.all_dates.findById(self.prevDate(aDateID)).per_pay_day;
-    return {
-        start   : prevDate,
-        end     : curDate
+    /*
+     * Вывод предыдущей даты
+     * @param {type} parDateID
+     * @returns {@exp;per_date}
+     * TODO Доделать добавление,запутался куда добавлять;
+     */
+    self.prevDate = function(aDateID) {
+        self.all_dates.scrollTo(self.all_dates.findById(aDateID));
+        self.all_dates.prev();
+        return self.all_dates.per_date_id;
     };
-};
 
-self.newDate = function(aCallBack) {
-    var progress = new ProgressShow();
-    progress.setMax(model.dsGroups.length * 4 + 1);
-    progress.setDescription("Подождите. Производится перенос данных в новый расчетный период");
-    callBack = aCallBack;
-    model.all_dates.last();
-    var prevDate = model.all_dates.cursor.per_date_id;
-    var lastDate = model.all_dates.per_date;
-    model.all_dates.cursor.edit_date = false;
-    model.all_dates.insert(self.all_dates.schema.per_date, lastDate.setMonth(lastDate.getMonth()+1), 
-                          self.all_dates.schema.edit_date, true );
-    var newDate = model.all_dates.cursor.per_date_id;
-    (function(){
-        model.dsAllAccounts.requery(function(){
-            model.dsAllAccounts.forEach(function(aAccount){             
-                    model.save(function(){
-            (function(){progress.increaseValue(1);}).invokeAndWait();
-           
-            model.dsGroups.beforeFirst();
-            while (model.dsGroups.next()) {
-                var group = model.dsGroups.grp_groups_id;
-                model.sums_4create.params.dateid = newDate;
-                model.sums_4create.params.groupid = group;
-                model.sums_4create.executeUpdate();
-                (function(){progress.increaseValue(1);}).invokeAndWait();
-                grpModules[group] = new NewMonthInitializer4Group(prevDate, newDate, group, self, progress, aAccount.grp_account_id);
-            }
-            
-            
-                var group = model.dsGroups.grp_groups_id;
-          //      model.sums_4create.params.dateid = newDate;
-         //       model.sums_4create.params.groupid = group;
-         //       model.sums_4create.executeUpdate();
-               (function(){progress.increaseValue(1);}).invokeAndWait();
-                grpModules[group] = new NewMonthInitializer4Lc(prevDate, newDate, self, progress, aAccount.grp_account_id);
-            
-            
-        });
-        });
-        });
-        (function(){progress.close();}).invokeAndWait();
-    }).invokeBackground();
-    progress.showModal();
-    self.model.params.prevDate = prevDate;
-    self.model.params.newDate = newDate;
-    //model.new_counter.execute();
-    //model.new_saldo.execute();
-    //model.new_tarif.execute();
-};
+    self.getLastDate = function() {
+        self.model.all_dates.last();
+        return self.model.all_dates.cursor.per_date_id;
+    };
 
-self.ready = function() {
-    grpProcessed++;
-    if (grpProcessed === model.dsGroups.length) {
-        grpModules = [];
-        callBack();
-    }
-};
+    self.getPayPeriod4Date = function(aDateID) {
+        var curDate = self.all_dates.findById(aDateID).per_pay_day;
+        var prevDate = self.all_dates.findById(self.prevDate(aDateID)).per_pay_day;
+        return {
+            start: prevDate,
+            end: curDate
+        };
+    };
+
+    self.newDate = function(aCallBack) {
+        var progress = new ProgressShow();
+        progress.setMax(model.dsGroups.length * 4 + 1);
+        progress.setDescription("Подождите. Производится перенос данных в новый расчетный период");
+        callBack = aCallBack;
+        model.all_dates.last();
+        var prevDate = model.all_dates.cursor.per_date_id;
+        var lastDate = model.all_dates.per_date;
+        model.all_dates.cursor.edit_date = false;
+        model.all_dates.insert(self.all_dates.schema.per_date, lastDate.setMonth(lastDate.getMonth() + 1),
+                self.all_dates.schema.edit_date, true);
+        var newDate = model.all_dates.cursor.per_date_id;
+        (function() {
+            model.dsAllAccounts.requery(function() {
+                model.dsAllAccounts.forEach(function(aAccount) {
+                    model.save(function() {
+                        (function() {
+                            progress.increaseValue(1);
+                        }).invokeAndWait();
+
+                        model.dsGroups.beforeFirst();
+                        while (model.dsGroups.next()) {
+                            var group = model.dsGroups.grp_groups_id;
+                            model.sums_4create.params.dateid = newDate;
+                            model.sums_4create.params.groupid = group;
+                            model.sums_4create.executeUpdate();
+                            (function() {
+                                progress.increaseValue(1);
+                            }).invokeAndWait();
+                            grpModules[group] = new NewMonthInitializer4Group(prevDate, newDate, group, self, progress, aAccount.grp_account_id);
+                        }
+
+
+                        var group = model.dsGroups.grp_groups_id;
+                        //      model.sums_4create.params.dateid = newDate;
+                        //       model.sums_4create.params.groupid = group;
+                        //       model.sums_4create.executeUpdate();
+                        (function() {
+                            progress.increaseValue(1);
+                        }).invokeAndWait();
+                        grpModules[group] = new NewMonthInitializer4Lc(prevDate, newDate, self, progress, aAccount.grp_account_id);
+                    });
+                });
+            });
+            (function() {
+                progress.close();
+            }).invokeAndWait();
+        }).invokeBackground();
+        progress.showModal();
+        self.model.params.prevDate = prevDate;
+        self.model.params.newDate = newDate;
+        //model.new_counter.execute();
+        //model.new_saldo.execute();
+        //model.new_tarif.execute();
+    };
+
+    self.ready = function() {
+        grpProcessed++;
+        if (grpProcessed === model.dsGroups.length) {
+            grpModules = [];
+            callBack();
+        }
+    };
 
 }
