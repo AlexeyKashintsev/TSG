dd636a2986cae50ae4e54a052922431cedef4aeb
Alexey Kashintsev <akashintsev@yandex.ru>
2015-04-14 14:37:56 +0300

    поправил запрос оплаты в сессии для правильно позиционирования курсора

--- app/Оплата/Запросы/Оплаты в сессии.sql
+++ app/Оплата/Запросы/Оплаты в сессии.sql
@@ -5,7 +5,9 @@
  * @writable opl_payments
  * @public
  */ 
-Select * 
+Select t1.opl_payments_id, t1.session_id, t1.flat_id, t1.payment_sum, t1.date_id
+, t1.payment_date, t1.payment_comment, t1.bank_percent, t1.full_payment, t.lc_regto
+, t.lc_flatnumber, t.registered_count, t.lc_num
 From opl_payments t1
  Inner Join lc_flat t on t1.flat_id = t.lc_flat_id
  Where :sessionid = t1.session_id
