d63eaeec4d7e346220b1282672a0ab587234b37b
Alexey Kashintsev <akashintsev@yandex.ru>
2015-04-21 10:18:36 +0300

    Агрегировал счетчики в расчетах

--- app/Расчеты/Запросы/Начальные показания счетчиков в квартирах.sql
+++ app/Расчеты/Запросы/Начальные показания счетчиков в квартирах.sql
@@ -4,7 +4,7 @@
  * @name lc_beg_counter_values_4calc
  * @manual 
  */ 
-Select t.lc_id, t.lc_flat_services_id, t2.beg_val AS fm_value
+Select t.lc_id, t.lc_flat_services_id, sum(t2.beg_val) AS fm_value
 , 'BEG_' || t4.cnt_type AS fm_name, t.services_id 
 From lc_flat_services t
  Inner Join cnt_con2flats t1 on t.lc_flat_services_id = t1.flat_service
@@ -12,3 +12,4 @@ From lc_flat_services t
  Inner Join cnt_counters t4 on t1.counter_id = t4.cnt_counters_id
  Where :dateid = t2.date_id
  and :lc_id = t.lc_id
+group by t.lc_id, t.lc_flat_services_id, t4.cnt_type, t.services_id 
