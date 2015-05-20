0139d15a6c0649ba322f6336c3408f4e72d2eb1a
Alexey Kashintsev <akashintsev@yandex.ru>
2015-05-20 17:13:01 +0400

    Поправил счетчики

--- app/Лицевые счета/Запросы/Значение счетчиков в квартире.sql
+++ app/Лицевые счета/Запросы/Значение счетчиков в квартире.sql
@@ -14,7 +14,8 @@ From counters_by_flat q1
  Inner Join per_counter_values t on q1.counter_id = t.counter_id
  Inner Join grp_services t2 on q1.services_id = t2.services_id
  and :parAccount = t2.account_id
- Left Join grp_service_counters t4 on q1.group_counter = t4.grp_service_counters_id
+ Inner Join grp_service_counters t4 on q1.group_counter = t4.grp_service_counters_id
+ and t4.grp_service_id = t2.grp_services_id
  Where :date_id = t.date_id
  and t2.askforvalue = true
  Order by t2.grp_services_id
