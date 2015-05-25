e4633eb751c4157d40352e3f96572d92554ead82
Alexey Kashintsev <akashintsev@yandex.ru>
2015-05-25 18:18:59 +0400

    Порядок услуг еще поправил

--- app/Лицевые счета/Запросы/Начисления по квартире.sql
+++ app/Лицевые счета/Запросы/Начисления по квартире.sql
@@ -9,7 +9,7 @@ Select *
 From (Select t1.per_sums_id, t1.flat_service_id, t1.date_id
 , t1.calc, t1.benefit, t1.recalc
 , t1.full_calc, t1.rate, t1.calc_value
-, t.lc_flat_services_id, t.lc_id, t.services_id, t3.usl_order
+, t.lc_flat_services_id, t.lc_id, t.services_id, max(t3.usl_order) as usl_order
 , t.fs_active, max(t3.grp_services_id) AS grp_services_id 
 From per_sums t1
  Left Join lc_flat_services t on t1.flat_service_id = t.lc_flat_services_id
@@ -21,5 +21,5 @@ From per_sums t1
  and :parAccount = t.account_id
  Group by t1.per_sums_id, t1.flat_service_id, t1.date_id, t1.calc, t1.benefit
 , t1.recalc, t1.full_calc, t1.rate, t1.calc_value, t.lc_flat_services_id
-, t.lc_id, t.services_id, t.fs_active, t3.usl_order) q
+, t.lc_id, t.services_id, t.fs_active) q
  Order by q.usl_order
