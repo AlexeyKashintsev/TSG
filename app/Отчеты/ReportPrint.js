54a521732809e5115183b684fd829a9313bd075f
Alexey Kashintsev <akashintsev@yandex.ru>
2015-06-04 16:04:07 +0400

    Поправил report print + добавил отчет для Надежды

--- app/Отчеты/ReportPrint.js
+++ app/Отчеты/ReportPrint.js
@@ -9,22 +9,33 @@ function ReportPrint() {
     //var repBillKapRemont = new Report(reportName);//BillsBuilder_Doverie_kapRemont();
     
     function process(aPrint) {
+        repBill = new Report(model.dsAllAccounts.find(model.dsAllAccounts.schema.grp_account_id, model.params.AccountID)[0].report_name);
         if (model.params.FlatID) {
             processSingleFlat(aPrint, model.params.FlatID);
         } else {
-            model.flats_by_group.beforeFirst();
-            if (confirm('Вы собираетесь открыть '+ (model.params.flatTo - model.params.flatFrom + 1) +' файлов'))
-            {while (model.flats_by_group.next())
-                if ((!model.params.flatFrom && !model.params.flatTo) || 
-                   ((model.params.flatFrom <= model.flats_by_group.lc_flatnumber) && (model.params.flatTo >= model.flats_by_group.lc_flatnumber)))
-                        processSingleFlat(aPrint, model.flats_by_group.lc_flat_id);
-             }
+            if (!repBill.all_flats) {
+                model.flats_by_group.beforeFirst();
+                if (confirm('Вы собираетесь открыть '+ (model.params.flatTo - model.params.flatFrom + 1) +' файлов'))
+                    model.flats_by_group.forEach(function(flat) {
+                        if ((!model.params.flatFrom && !model.params.flatTo) || 
+                           ((model.params.flatFrom <= flat.lc_flatnumber) && (model.params.flatTo >= flat.lc_flatnumber)))
+                                processSingleFlat(aPrint, flat.lc_flat_id);
+                    });
+                }
+            else {
+                repBill.model.params.parDateID = self.params.parDateID;
+                repBill.model.params.parGroupID = self.params.GroupID; 
+                repBill.model.params.parAccountID = self.params.AccountID;
+                if (aPrint) 
+                    repBill.print();
+                else
+                    repBill.show();
+            }
         }
     }
     
     function processSingleFlat(aPrint, aFlatID) {
        // if (model.params.AccountID == 142486607303119){
-            repBill = new Report(model.dsAllAccounts.find(model.dsAllAccounts.schema.grp_account_id, model.params.AccountID)[0].report_name);
             repBill.model.params.parDateID = self.params.parDateID;
             repBill.model.params.parFlatID = aFlatID; 
             repBill.model.params.parAccountID = self.params.AccountID;
