138e0e76555b3a286fb0dbdd1e3cfdc6d9efdb7d
Alexey Kashintsev <ak@altsoft.biz>
2015-11-20 14:00:46 +0400

    tune querry - начисления по группе

--- app/Группы/Запросы/Статистика/Начисления по группе.sql
+++ app/Группы/Запросы/Статистика/Начисления по группе.sql
@@ -8,10 +8,10 @@
 Select t2.services_id, sum(t.calc) AS calc, sum(t.benefit) AS benefit
 , sum(t.recalc) AS recalc, sum(t.full_calc) AS full_calc, sum(t.calc_value) AS calc_value
 , t3.usl_name 
-From grp_lc_group t1
- Inner Join lc_flat_services t2 on t1.lc_id = t2.lc_id
- Inner Join per_sums t on t2.lc_flat_services_id = t.flat_service_id
- Inner Join #DateGroupView q on q.per_date_id = t.date_id
+From #DateGroupView q
+ Inner Join per_sums t on q.per_date_id = t.date_id
+ Inner Join lc_flat_services t2 on t2.lc_flat_services_id = t.flat_service_id
+ Inner Join grp_lc_group t1 on t1.lc_id = t2.lc_id
  Left Join usl_services t3 on t2.services_id = t3.usl_services_id
  Left Join grp_services t4 on t2.services_id = t4.services_id
          and t1.group_id = t4.group_id and :parAccount = t4.account_id
