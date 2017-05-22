6907ed160a662930ddf11e2ec080ed93f8fb13ef
sa <sa@altsoft.biz>
2017-05-22 11:00:02 +0300

    Fix

--- app/Группы/Отчеты/moduleReportName.js
+++ app/Группы/Отчеты/moduleReportName.js
@@ -27,6 +27,7 @@ function moduleReportName() {
         return [
             {report_name_view: "Доверие Общий", report_name: "BillsBuilder_Doverie"},
             {report_name_view: "Интерстрой Общий", report_name: "BillsBuilder_Interstroy"},
+            {report_name_view: "Интерстрой Сакко", report_name: "BillsBuilder_Interstroy_Sakko"},
             {report_name_view: "Доверие Капремонт", report_name: "BillsBuilder_Doverie_kapRemont"},
             {report_name_view: "Наш Дом Капремонт", report_name: "BillsBuilder_NashDom_kapRemont"},
             {report_name_view: "Наш Дом Общий", report_name: "BillsBuilder_Nash_Dom"},
